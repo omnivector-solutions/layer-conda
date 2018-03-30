d41ad9b908a6d53c586d16101505d79b7d00a02a
jamesbeedy <jamesbeedy@gmail.com>
2018-03-29 21:00:02 -0700

    use conda-api

--- reactive/conda.py
+++ reactive/conda.py
@@ -1,60 +1,36 @@
-import subprocess
-
-from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import log, status_set
 from charmhelpers.core.host import chownr
-from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
+
 from charms.reactive import endpoint_from_flag, when, when_not, set_flag
+
+from charms.layer.conda_api import init_install_conda, CONDA_HOME
 
 
 @when('endpoint.conda.available')
 @when_not('conda.installed')
 def install_conda():
-    aufh = ArchiveUrlFetchHandler()
+    log("Installing Conda")
+    status_set('maintenance', "Installing Conda")
+
     endpoint = endpoint_from_flag('endpoint.conda.joined')
     conda_data = endpoint.relation_data()[0]
 
-    hookenv.log("Install Conda")
-    hookenv.status_set('maintenance', "Installing Conda")
-
-    # Download and install conda
-    conda_installer_path = aufh.download_and_validate(
-        conda_data.get('url'),
-        conda_data.get('sha'),
-        validate="sha256"
+    init_install_conda(
+        conda_data['url'],
+        conda_data['sha'],
+        validate='sha256'
     )
-    if conda_installer_path:
-        subprocess.call(['bash', conda_installer_path, '-b', '-f', '-p',
-                         '/home/ubuntu/anaconda'])
-        subprocess.call(['/home/ubuntu/anaconda/bin/conda', 'update', '-y',
-                         '-n', 'base', 'conda'])
 
-        # Create virtualenv and install jupyter
-        subprocess.call(['/home/ubuntu/anaconda/bin/conda', 'create', '-y',
-                         '-n', 'jupyter', 'python=3.5', 'jupyter', 'nb_conda'])
-        subprocess.call(['/home/ubuntu/anaconda/bin/conda', 'install', '-y',
-                         'jupyter'])
+    create_conda_venv(python_version="3.5")
+    chownr(CONDA_HOME, 'ubuntu', 'ubuntu', chowntopdir=True)
 
-        # Install any extra conda packages
-        if conda_data.get('conda_extra_packages'):
-            subprocess.call(['/home/ubuntu/anaconda/bin/conda', 'install',
-                             '-y', conda_data.get('conda_extra_packages')])
+    log("Conda installed")
+    status_set('active', "Conda installed")
+    set_flag('conda.installed')
 
-        # Pip install findspark
-        subprocess.call(['/home/ubuntu/anaconda/envs/jupyter/bin/pip',
-                         'install', 'findspark'])
 
-        # Install any extra conda pip packages
-        if conda_data.get('conda_extra_pip_packages'):
-            subprocess.call(
-                ['/home/ubuntu/anaconda/envs/jupyter/bin/pip',
-                 'install', conda_data.get('conda_extra_pip_packages')])
-        # Chown the perms to ubuntu
-        chownr('/home/ubuntu/anaconda', 'ubuntu', 'ubuntu', chowntopdir=True)
-
-        # Set installed flag
-        set_flag('conda.installed')
-        hookenv.status_set('active', "Conda ready")
-    else:
-        hookenv.status_set('blocked',
-                           "Could not verify conda installer, please DEBUG")
-        return
+@when_not('endpoint.conda.available')
+def status_blocked():
+    hookenv.status_set('blocked',
+                       "Need relation to conda provider to continue.")
+    return
